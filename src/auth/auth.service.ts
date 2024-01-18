6188bee787a0c427d6e98fa774800750396cfe3f
Luís Fernando Dandolini Duarte <lduarte@personalcard.com.br>
2024-01-18 15:46:42 -0300

    feat: auth implements local strategy validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,24 @@
+import { UserService } from './../user/user.service';
 import { Injectable } from '@nestjs/common';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
-  validateUser(email: string, password: string) {
-    throw new Error('Method not implemented.');
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      const isPasswordValue = await bcrypt.compareSync(password, user.password);
+
+      if (isPasswordValue) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+
+    throw new Error('Email ou senha inválidos');
   }
 }
