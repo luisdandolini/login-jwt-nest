163a7534f1f19c932771483ead7dd11e60da5d00
Luís Fernando Dandolini Duarte <lduarte@personalcard.com.br>
2024-01-18 13:47:04 -0300

    feat: config prisma

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    // Use type casting to satisfy TypeScript
+    (this.$on as any)('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
