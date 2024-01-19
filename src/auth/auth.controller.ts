7118151a6559c54a72ce026d0fa094fedf8b34e9
Luís Fernando Dandolini Duarte <lduarte@personalcard.com.br>
2024-01-19 08:46:22 -0300

    feat: create login and generate jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,9 +4,11 @@ import {
   HttpStatus,
   Post,
   UseGuards,
+  Request,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './models/AuthRequest';
 
 @Controller()
 export class AuthController {
@@ -15,7 +17,9 @@ export class AuthController {
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
-  login() {
-    return 'Realizar login';
+  login(@Request() req: AuthRequest) {
+    console.log(req.user);
+
+    return this.authService.login(req.user);
   }
 }
